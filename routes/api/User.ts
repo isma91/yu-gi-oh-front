1bce221a344d3a3ae3f150078877fc071dd7d545
isma91 <ismaydogmus@gmail.com>
2024-03-06 17:26:49 +0100

    create User route for APi and front

---
+++ routes/api/User.ts
@@ -0,0 +1,19 @@
+export enum UserApiRouteName {
+    CREATE = "create",
+    GET_ALL = "get_all",
+    LOGIN = "login",
+    REFRESH_LOGIN = "refresh-login",
+}
+
+const USER_API_ROUTE_JSON = {
+    [UserApiRouteName.CREATE]: "/create",
+    [UserApiRouteName.GET_ALL]: "/all",
+    [UserApiRouteName.LOGIN]: "/login",
+    [UserApiRouteName.REFRESH_LOGIN]: "/refresh-login",
+};
+
+export const USER_API_BASE_URL = "/user";
+
+export function GetFullRoute(name: UserApiRouteName): string {
+    return `${USER_API_BASE_URL}${USER_API_ROUTE_JSON[name]}`;
+}
