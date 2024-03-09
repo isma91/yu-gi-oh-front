504ba5d51e51571e9eb5ddd01f6ee61becafab57
isma91 <ismaydogmus@gmail.com>
2024-03-09 13:17:24 +0100

    add title in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import DashboardHome from "@components/dashboard/Home";
 
 export default function Home() {
-    return <DashboardHome active={0}>index page</DashboardHome>;
+    return (
+        <DashboardHome active={0} title="Home page">
+            index page
+        </DashboardHome>
+    );
 }
