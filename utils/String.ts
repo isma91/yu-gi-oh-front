27b8991d120410e7742237d3a04894ad89230216
isma91 <ismaydogmus@gmail.com>
2024-03-09 13:13:51 +0100

    create url tool function + add a limit text for string tool

--- utils/String.ts
+++ utils/String.ts
@@ -7,42 +7,19 @@ export function Capitalize(string: string): string {
 }
 
 /**
- * @param {string | number} string 
- * @param {number} [length=2] 
+ * @param {string} text 
+ * @param {number} length 
  * @returns {string}
  */
-export function FulfillZero(string: string | number, length: number = 2): string {
-    if (length < 2) {
-        length = 2;
+export function LimitText(text: string, length: number): string {
+    if (!text || !length || text.length <= length) {
+        return text;
     }
-    return string.toString().padStart(length, "0")
-}
-
-/**
- * @param {string} base 
- * @param {number} index 
- * @returns {string}
- */
-export function AddIndexForFieldMultiple(base: string, index: number): string {
-    return `${base}[${index}]`;
-}
-
-/**
- * @param string 
- * @returns {[number | null, string | null]}
- */
-export function SplitNumberAndLetter(string: string): [number | null, string | null] {
-    let responseNumber: number | null = null;
-    let responseLetter: string | null = null;
-    const regexNumber = new RegExp(/[0-9]+/g);
-    const regexLetter = new RegExp(/[a-zA-Z]+/g);
-    const matchNumberArray = string.match(regexNumber);
-    const matchLetterArray = string.match(regexLetter);
-    if (matchNumberArray !== null) {
-        responseNumber = parseInt(matchNumberArray[0], 10);
+    let limitted = text.slice(0, length).trim();
+    const index = limitted.lastIndexOf(" ");
+    if (index !== -1) {
+        limitted = limitted.slice(0, index);
     }
-    if (matchLetterArray !== null) {
-        responseLetter = matchLetterArray[0];
-    }
-    return [responseNumber, responseLetter];
+    limitted = limitted.replace(/[.,]*$/g, "");
+    return `${limitted}...`;
 }
