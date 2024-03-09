f090b0e0dc1809bdb50044cc022fb17fdf30c1ae
isma91 <ismaydogmus@gmail.com>
2024-03-09 13:18:19 +0100

    create search card page

---
+++ pages/card/card-search.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import DashboardHome from "@components/dashboard/Home";
+import { Grid } from "@mui/material";
+import CardSearchForm from "@form/card/search";
+import { CardSearchType } from "@app/types/entity/Card";
+import SearchLimit from "@components/specialField/SearchLimit";
+import SearchCardDisplay from "@components/specialField/SearchCardDisplay";
+
+export default function CardSearchPage() {
+    const [offset, setOffset] = useState<number>(0);
+    const [limit, setLimit] = useState<number>(15);
+    const [cardAllResultCount, setCardAllResultCount] = useState<number>(0);
+    const [cardResult, setCardResult] = useState<CardSearchType[]>([]);
+    const limitArray: number[] = [15, 30, 45, 60];
+
+    return (
+        <DashboardHome active={1} activeChild={0} title="Card Search Page">
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <CardSearchForm
+                        offsetState={[offset, setOffset]}
+                        setCardAllResultCount={setCardAllResultCount}
+                        setCardResult={setCardResult}
+                        limit={limit}
+                        searchLimit={<SearchLimit name="limit" valueArray={limitArray} limitState={[limit, setLimit]} />}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <SearchCardDisplay
+                        offsetState={[offset, setOffset]}
+                        cardResult={cardResult}
+                        limit={limit}
+                        cardAllResultCount={cardAllResultCount}
+                    />
+                </Grid>
+            </Grid>
+        </DashboardHome>
+    );
+}
