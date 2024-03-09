bada1062245e82886f0928d2adbb09e6c463427a
isma91 <ismaydogmus@gmail.com>
2024-03-09 13:12:53 +0100

    add redirect for parent menu

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/card",
+                destination: "/card/card-search",
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
