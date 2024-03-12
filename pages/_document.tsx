e252937c6ef7df70335794229cb562a0fae28a47
isma91 <ismaydogmus@gmail.com>
2024-03-12 14:58:50 +0100

    create a custom document where we put metadata

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="isma91 Yu-Gi-Oh! front page" />
+                <title>Yu-Gi-Oh!</title>
+            </head>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
