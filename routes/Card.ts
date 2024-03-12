b00b82af6d9cbc5dfa0063e217095ed05e01f3f6
isma91 <ismaydogmus@gmail.com>
2024-03-12 20:19:55 +0100

    add card route api + front

--- routes/Card.ts
+++ routes/Card.ts
@@ -1,11 +1,25 @@
+import { GetFullRouteFromRouteNameAndRouteOption } from "@utils/Route";
 export enum CardRouteName {
     CARD_SEARCH = "card-search",
     SET_SEARCH = "set-search",
+    CARD_INFO = "card-info",
 }
 
 export const CARD_ROUTE_JSON = {
     [CardRouteName.CARD_SEARCH]: "/card-search",
     [CardRouteName.SET_SEARCH]: "/set-search",
+    [CardRouteName.CARD_INFO]: "/info/{uuid}/{slugName}",
 };
 
 export const CARD_BASE_URL = "/card";
+
+type CardRouteOptionKey = "uuid" | "slugName";
+
+type CardRouteOption = {
+    [key in CardRouteOptionKey]: string;
+}
+
+export function GetFullRoute(cardRouteName: CardRouteName, option: CardRouteOption | null = null): string {
+    let url = `${CARD_BASE_URL}${CARD_ROUTE_JSON[cardRouteName]}`;
+    return GetFullRouteFromRouteNameAndRouteOption<CardRouteOption>(url, option);
+}
---
+++ routes/api/Card.ts
@@ -0,0 +1,22 @@
+import { GetFullRouteFromRouteNameAndRouteOption } from "@utils/Route";
+
+export enum CardApiRouteName {
+    GET_INFO = "get-info",
+}
+
+const CARD_API_ROUTE_JSON = {
+    [CardApiRouteName.GET_INFO]: "/info/{uuid}",
+};
+
+export const CARD_API_BASE_URL = "/card";
+
+type CardApiRouteOptionKey = "uuid";
+
+type CardApiRouteOption = {
+    [key in CardApiRouteOptionKey]: string;
+}
+
+export function GetFullRoute(name: CardApiRouteName, option: CardApiRouteOption | null = null): string {
+    let url = `${CARD_API_BASE_URL}${CARD_API_ROUTE_JSON[name]}`;
+    return GetFullRouteFromRouteNameAndRouteOption(url, option);
+}
