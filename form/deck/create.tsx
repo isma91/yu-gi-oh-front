9e8d368893fc4c14fcbd9871de45a1c257e4f2fc
isma91 <ismaydogmus@gmail.com>
2024-03-17 23:48:35 +0100

    add propr to send request

--- form/deck/create.tsx
+++ form/deck/create.tsx
@@ -5,8 +5,12 @@ import { Box, Grid, Theme } from "@mui/material";
 import InputText from "@components/field/InputText";
 import Switch from "@components/field/Switch";
 import Button from "@components/field/Button";
-import { SelectDeckArtowrkType } from "@app/types/Deck";
+import { DeckCardFieldType, DeckCardType, SelectDeckArtowrkType } from "@app/types/Deck";
 import Autocomplete from "@components/field/Autocomplete";
+import DeckCreateRequest from "@api/deck/Create";
+import { useSnackbar } from "notistack";
+import { useRouter } from "next/router";
+import { DeckRouteName, GetFullRoute } from "@routes/Deck";
 
 type ErrorsType = {
     [key in string]: string | undefined;
@@ -18,6 +22,7 @@ type ValuesType = {
 
 type DeckCreateForm = {
     selectDeckArtowrkArray: SelectDeckArtowrkType[];
+    deckCard: DeckCardType;
 };
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -28,17 +33,63 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export default function DeckCreateForm(props: DeckCreateForm) {
-    const { selectDeckArtowrkArray } = props;
+    const { selectDeckArtowrkArray, deckCard } = props;
+    const { enqueueSnackbar } = useSnackbar();
+    const router = useRouter();
     const classes = useStyles();
     const [values, setValues] = useState<ValuesType>({});
     const [errors, setErrors] = useState<ErrorsType>({});
     const [loading, setLoading] = useState<boolean>(false);
 
+    const sendDeckCreateReq = async (data: object) => {
+        return DeckCreateRequest(data)
+            .then((res) => {
+                enqueueSnackbar(res.success, { variant: "success" });
+                router.push(GetFullRoute(DeckRouteName.LIST));
+            })
+            .catch((err) => {
+                enqueueSnackbar(err, { variant: "error" });
+                setLoading(false);
+            });
+    };
+
+    const transformValues = () => {
+        const deckCardFieldTypeArray = Object.values(DeckCardFieldType);
+        let deckCardUniqueJson: { [key in DeckCardFieldType]: { [key in number]: number } } = {
+            [DeckCardFieldType.MAIN_DECK]: {},
+            [DeckCardFieldType.EXTRA_DECK]: {},
+            [DeckCardFieldType.SIDE_DECK]: {},
+        };
+        deckCardFieldTypeArray.forEach((cardFieldType) => {
+            deckCard[cardFieldType].forEach((cardInfo) => {
+                const { id: cardInfoId } = cardInfo;
+                if (deckCardUniqueJson[cardFieldType][cardInfoId] === undefined) {
+                    deckCardUniqueJson[cardFieldType][cardInfoId] = 1;
+                } else {
+                    deckCardUniqueJson[cardFieldType][cardInfoId] = deckCardUniqueJson[cardFieldType][cardInfoId] + 1;
+                }
+            });
+        });
+        let newDeckCardValues: { [key in string]: string } = {};
+        deckCardFieldTypeArray.forEach((cardFieldType) => {
+            Object.keys(deckCardUniqueJson[cardFieldType]).forEach((cardInfoIdString, index) => {
+                const cardInfoId = parseInt(cardInfoIdString, 10);
+                const nbCopie = deckCardUniqueJson[cardFieldType][cardInfoId];
+                const newDeckCardValuesBaseKey = `[deck-card][${cardFieldType}][${index}]`;
+                newDeckCardValues[`${newDeckCardValuesBaseKey}[id]`] = cardInfoId.toString(10);
+                newDeckCardValues[`${newDeckCardValuesBaseKey}[nbCopie]`] = nbCopie.toString(10);
+            });
+        });
+        return { ...values, ...newDeckCardValues };
+    };
+
     useEffect(() => {
         const valuesLength = Object.keys(values).length;
         const errorsLength = Object.keys(errors).length;
         if (valuesLength > 0 && errorsLength === 0) {
-            console.log(values);
+            setLoading(true);
+            const newValues = transformValues();
+            sendDeckCreateReq(newValues);
         }
     }, [values, errors]);
 
