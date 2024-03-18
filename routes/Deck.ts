81bd85125bc47d134bb71a3e31b4b829c99622c9
isma91 <ismaydogmus@gmail.com>
2024-03-18 21:27:25 +0100

    add new route

--- routes/Deck.ts
+++ routes/Deck.ts
@@ -3,16 +3,24 @@ import { GetFullRouteFromRouteNameAndRouteOption } from "@utils/Route";
 export enum DeckRouteName {
     LIST = "list",
     CREATE = "create",
+    INFO = "info",
 }
 
 export const DECK_ROUTE_JSON = {
     [DeckRouteName.LIST]: "/list",
     [DeckRouteName.CREATE]: "/create",
+    [DeckRouteName.INFO]: "/info/{uuid}/{slugName}",
 };
 
 export const DECK_BASE_URL = "/deck";
 
-export function GetFullRoute(deckRouteName: DeckRouteName, option: null = null): string {
+type DeckRouteOptionKey = "uuid" | "slugName";
+
+type DeckRouteOption = {
+    [key in DeckRouteOptionKey]: string;
+}
+
+export function GetFullRoute(deckRouteName: DeckRouteName, option: null | DeckRouteOption = null): string {
     let url = `${DECK_BASE_URL}${DECK_ROUTE_JSON[deckRouteName]}`;
-    return GetFullRouteFromRouteNameAndRouteOption(url, option);
+    return GetFullRouteFromRouteNameAndRouteOption<DeckRouteOption>(url, option);
 }
