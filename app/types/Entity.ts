e22a771d3a5afea5464add3b08fcc355c101d4ec
isma91 <ismaydogmus@gmail.com>
2024-03-19 14:12:35 +0100

    add new type as entity generic

--- app/types/Entity.ts
+++ app/types/Entity.ts
@@ -1,3 +1,6 @@
+import { CardEntityType, CardSearchType } from "@app/types/entity/Card";
+import { DeckEntityType } from "@app/types/entity/Deck";
+
 export interface TimestampableEntity {
     createdAt: Date | DateStringType;
     updatedAt: Date | DateStringType;
@@ -11,3 +14,16 @@ export interface NameSlugNameEntity {
 export type DateStringType = string;
 
 export type UuidStringType = string;
+
+export type CardDeckEntityType = TimestampableEntity & NameSlugNameEntity & {
+    id: number;
+    cards: CardEntityType[];
+    nbCopie: number;
+    deck: DeckEntityType;
+};
+
+export type CardDeckeGetInfoType = NameSlugNameEntity & {
+    id: number;
+    nbCopie: number;
+    cards: CardSearchType[],
+}
