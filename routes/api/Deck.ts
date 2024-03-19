45c96b11ed835dc1e8b297ca3b463b539dc6ba9c
isma91 <ismaydogmus@gmail.com>
2024-03-19 14:13:36 +0100

    create delete route api

--- routes/api/Deck.ts
+++ routes/api/Deck.ts
@@ -1,14 +1,27 @@
+import { GetFullRouteFromRouteNameAndRouteOption } from "@utils/Route";
+
 export enum DeckApiRouteName {
     CREATE = "create",
+    GET_INFO = "info",
+    DELETE = "delete",
     
 }
 
 const DECK_API_ROUTE_JSON = {
     [DeckApiRouteName.CREATE]: "/create",
+    [DeckApiRouteName.GET_INFO]: "/info/{id}",
+    [DeckApiRouteName.DELETE]: "/delete/{id}",
 };
 
 export const DECK_API_BASE_URL = "/deck";
 
-export function GetFullRoute(name: DeckApiRouteName): string {
-    return `${DECK_API_BASE_URL}${DECK_API_ROUTE_JSON[name]}`;
+type DeckApiRouteOptionKey = "id";
+
+type DeckApiRouteOption = {
+    [key in DeckApiRouteOptionKey]: string;
+}
+
+export function GetFullRoute(name: DeckApiRouteName, option: DeckApiRouteOption | null = null): string {
+    let url = `${DECK_API_BASE_URL}${DECK_API_ROUTE_JSON[name]}`;
+    return GetFullRouteFromRouteNameAndRouteOption(url, option);
 }
