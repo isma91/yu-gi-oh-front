eee3fc83d8a10cc216d6b942fca9466cdf6ab2df
isma91 <ismaydogmus@gmail.com>
2024-03-19 14:54:40 +0100

    fix request type sended

--- api/Deck/Delete.ts
+++ api/Deck/Delete.ts
@@ -1,8 +1,8 @@
 import Request from "@api/Request";
 import { RequestMethodType } from "@app/types/Api";
-import { DeckGetInfoRequestType } from "@app/types/entity/Deck";
+import { RequestSuccessType } from "@app/types/Request";
 import { GetFullRoute, DeckApiRouteName } from "@routes/api/Deck";
 
-export default function DeckGetInfo(id: number): Promise<DeckGetInfoRequestType> {
-    return Request(GetFullRoute(DeckApiRouteName.GET_INFO, {id: id.toString(10)}), RequestMethodType.GET, "Error while getting deck.", null, true);
+export default function DeckDeleteFromId(id: number): Promise<RequestSuccessType> {
+    return Request(GetFullRoute(DeckApiRouteName.DELETE, {id: id.toString(10)}), RequestMethodType.DELETE, "Error while deleting deck.", null, true);
 }
