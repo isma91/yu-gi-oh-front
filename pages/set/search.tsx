05815a1bc70a60b44c7c2891b1c1680383e61506
isma91 <ismaydogmus@gmail.com>
2024-03-22 23:33:18 +0100

    display set search form + set Search display

--- pages/set/search.tsx
+++ pages/set/search.tsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 import DashboardHome from "@components/dashboard/Home";
 import { Grid } from "@mui/material";
-import CardSearchForm from "@form/card/search";
-import { CardSearchType } from "@app/types/entity/Card";
+import { SetGetAllType } from "@app/types/entity/Set";
+import SetSearchForm from "@form/set/search";
 import SearchLimitSelect from "@components/search/LimitSelect";
-import SearchCardDisplay from "@components/search/CardDisplay";
+import SearchSetDisplay from "@components/search/SetDisplay";
 
 export default function SetSearchPage() {
     const [offset, setOffset] = useState<number>(0);
-    const [limit, setLimit] = useState<number>(15);
-    const [cardAllResultCount, setCardAllResultCount] = useState<number>(0);
+    const [limit, setLimit] = useState<number>(30);
+    const [setAllResultCount, setSetAllResultCount] = useState<number>(0);
     const [loadingForm, setLoadingForm] = useState<boolean>(false);
-    const [cardResult, setCardResult] = useState<CardSearchType[]>([]);
-    const limitArray: number[] = [15, 30, 45, 60];
+    const [setResult, setSetResult] = useState<SetGetAllType[]>([]);
+    const limitArray: number[] = [30, 45, 60];
 
     return (
         <DashboardHome active={2} activeChild={0} title="Set Search Page">
-            <Grid container spacing={2}></Grid>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <SetSearchForm
+                        offsetState={[offset, setOffset]}
+                        loadingFormState={[loadingForm, setLoadingForm]}
+                        setSetAllResultCount={setSetAllResultCount}
+                        setSetResult={setSetResult}
+                        limit={limit}
+                        searchLimit={<SearchLimitSelect name="limit" valueArray={limitArray} limitState={[limit, setLimit]} />}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <SearchSetDisplay setResult={setResult} setAllResultCount={setAllResultCount} limit={limit} offsetState={[offset, setOffset]} />
+                </Grid>
+            </Grid>
         </DashboardHome>
     );
 }
