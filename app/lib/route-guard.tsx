4ffd7871d2fee16c63ad9255d9e267faed562c3a
isma91 <ismaydogmus@gmail.com>
2024-03-24 23:04:54 +0100

    add missing dependencies + add jwt + login page direclty in the useEffect because used only here

--- app/lib/route-guard.tsx
+++ app/lib/route-guard.tsx
@@ -6,6 +6,7 @@ import RefreshLoginRequest from "@api/User/Auth/RefreshLogin";
 import { ActionNameType, GlobalStateType, ActionType } from "@app/types/GlobalState";
 import { UserLoginRequestType } from "@app/types/entity/User";
 import { USER_ROUTE_JSON, UserRouteName } from "@routes/User";
+import { GetLocalStorageValue } from "@utils/Window";
 
 /**
  *
@@ -22,12 +23,6 @@ function RouteGuard({ children }: { children: React.ReactNode }): React.ReactNod
         dispatch: React.Dispatch<ActionType>;
     } = useContext(StoreContext);
     const router: NextRouter = useRouter();
-    const loginPath: string = USER_ROUTE_JSON[UserRouteName.LOGIN];
-    const NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY: string = process.env.NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY || "";
-    let jwt: string | null = null;
-    if (typeof window !== "undefined") {
-        jwt = window.localStorage.getItem(NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY);
-    }
 
     /**
      * if we have a JWT in the localStorage and no globalState we try to refresh the login first
@@ -36,10 +31,51 @@ function RouteGuard({ children }: { children: React.ReactNode }): React.ReactNod
      * we put the globalState as depandancy to avoid some error
      */
     useEffect(() => {
+        const loginPath: string = USER_ROUTE_JSON[UserRouteName.LOGIN];
+        const NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY: string = process.env.NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY || "";
+        let jwt: string | null = GetLocalStorageValue(NEXT_PUBLIC_LOCALSTORAGE_TOKEN_KEY);
         if (state.user === null && jwt !== null) {
-            refreshLogin();
+            RefreshLoginRequest()
+                .then((res: UserLoginRequestType) => {
+                    dispatch({ type: ActionNameType.Login, payload: res.data.userInfo });
+                })
+                .catch((err: string | null) => {
+                    console.error("refresh login error:");
+                    console.error(err);
+                    dispatch({ type: ActionNameType.Logout, payload: null });
+                });
         }
-        authCheck(router.asPath);
+        /**
+         * if not logged user try to go to a path who's not the login route, we redirect to login
+         * if logged user try to go to login page, we redirect to home page or the waited url before login
+         */
+        const authCheck = (): Promise<boolean> | void => {
+            const completePath: string[] = router.asPath.split("?");
+            const path: string = completePath[0];
+            let returnUrl: string | null = null;
+            if (completePath.length > 1) {
+                const returnUrlString = "returnUrl=";
+                if (completePath[1].startsWith(returnUrlString) === true) {
+                    returnUrl = decodeURIComponent(completePath[1].substring(returnUrlString.length));
+                }
+            }
+            const maybeLogged = state.user !== null || jwt !== null;
+            if (maybeLogged === false && path !== loginPath) {
+                router.push({
+                    pathname: loginPath,
+                    query: { returnUrl: router.asPath },
+                });
+            } else if (maybeLogged === true && path === loginPath) {
+                let url = "/";
+                if (returnUrl !== null) {
+                    url = returnUrl;
+                }
+                router.push(url);
+            } else {
+                setAuthorized(true);
+            }
+        };
+        authCheck();
         const hide = () => setAuthorized(false);
         router.events.on("routeChangeStart", hide);
         router.events.on("routeChangeComplete", authCheck);
@@ -47,46 +83,8 @@ function RouteGuard({ children }: { children: React.ReactNode }): React.ReactNod
             router.events.off("routeChangeStart", hide);
             router.events.off("routeChangeComplete", authCheck);
         };
-    }, [state]);
+    }, [state, router, dispatch]);
 
-    /**
-     * Try to login with the JWT in localStorage
-     * @returns {Promise<void>}
-     */
-    const refreshLogin = async (): Promise<void> => {
-        return RefreshLoginRequest()
-            .then((res: UserLoginRequestType) => {
-                dispatch({ type: ActionNameType.Login, payload: res.data.userInfo });
-            })
-            .catch((err: string | null) => {
-                console.error("refresh login error:");
-                console.error(err);
-                dispatch({ type: ActionNameType.Logout, payload: null });
-            });
-    };
-
-    /**
-     * if not logged user try to go to a path who's not the login route, we redirect to login
-     * if logged user try to go to login page, we redirect to home page
-     * @param {string} url
-     */
-    const authCheck = (url: string): Promise<boolean> | void => {
-        const path: string = url.split("?")[0];
-        /**
-         * we can only assume like that or when we change link manually we allways go to  the home page
-         */
-        const maybeLogged = state.user !== null || jwt !== null;
-        if (maybeLogged === false && path !== loginPath) {
-            router.push({
-                pathname: loginPath,
-                query: { returnUrl: router.asPath },
-            });
-        } else if (maybeLogged === true && path === loginPath) {
-            router.push("/");
-        } else {
-            setAuthorized(true);
-        }
-    };
     return authorized && children;
 }
 
