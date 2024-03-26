056c4a2b937c894e16ab43b1e28462d3413a5286
isma91 <ismaydogmus@gmail.com>
2024-03-26 14:16:09 +0100

    return all deck warning

--- utils/DeckCard.ts
+++ utils/DeckCard.ts
@@ -49,7 +49,7 @@ export function TransformDeckCardToValueRequest(deckCard: DeckCardType, values: 
     return { ...values, ...newDeckCardValues };
 }
 
-export function FindDeckCardWarning(deckCardNumberPerFieldType: DeckCardNumberPerFieldTypeType): string | null {
+export function FindAllDeckCardWarning(deckCardNumberPerFieldType: DeckCardNumberPerFieldTypeType): string[] {
     const nbCardMinMainDeck = parseInt(process.env["NEXT_PUBLIC_NB_MIN_CARD_MAIN_DECK"] as string, 10);
     const nbCardMaxMainDeck = parseInt(process.env["NEXT_PUBLIC_NB_MAX_CARD_MAIN_DECK"] as string, 10);
     const nbCardMaxExtraDeck = parseInt(process.env["NEXT_PUBLIC_NB_MAX_CARD_EXTRA_DECK"] as string, 10);
@@ -57,15 +57,15 @@ export function FindDeckCardWarning(deckCardNumberPerFieldType: DeckCardNumberPe
     const deckCardMainDeckNumber = deckCardNumberPerFieldType[DeckCardFieldType.MAIN_DECK];
     const deckCardExtraDeckNumber = deckCardNumberPerFieldType[DeckCardFieldType.EXTRA_DECK];
     const deckCardSideDeckNumber = deckCardNumberPerFieldType[DeckCardFieldType.SIDE_DECK];
-    let newDeckCardWarning = null;
+    let deckWarningArray = [];
     if (deckCardMainDeckNumber > nbCardMaxMainDeck) {
-        newDeckCardWarning = `The ${DeckCardFieldType.MAIN_DECK} have more than ${nbCardMaxMainDeck} cards !!`;
+        deckWarningArray.push(`The ${DeckCardFieldType.MAIN_DECK} have more than ${nbCardMaxMainDeck} cards !!`);
     } else if (deckCardMainDeckNumber < nbCardMinMainDeck) {
-        newDeckCardWarning = `The ${DeckCardFieldType.MAIN_DECK} have less than ${nbCardMinMainDeck} cards !!`;
+        deckWarningArray.push(`The ${DeckCardFieldType.MAIN_DECK} have less than ${nbCardMinMainDeck} cards !!`);
     } else if (deckCardExtraDeckNumber > nbCardMaxExtraDeck) {
-        newDeckCardWarning = `The ${DeckCardFieldType.EXTRA_DECK} have more than ${nbCardMaxExtraDeck} cards !!`;
+        deckWarningArray.push(`The ${DeckCardFieldType.EXTRA_DECK} have more than ${nbCardMaxExtraDeck} cards !!`);
     } else if (deckCardSideDeckNumber > nbCardMaxSideDeck) {
-        newDeckCardWarning = `The ${DeckCardFieldType.SIDE_DECK} have more than ${nbCardMaxSideDeck} cards !!`;
+        deckWarningArray.push(`The ${DeckCardFieldType.SIDE_DECK} have more than ${nbCardMaxSideDeck} cards !!`);
     }
-    return newDeckCardWarning;
+    return deckWarningArray;
 }
